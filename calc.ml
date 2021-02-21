c95096f44956762620e45473c9761037afea7e6a
Cam Miller <cm3959@columbia.edu>
2021-02-20 19:18:33 -0500

    Added calc.ml

---
+++ calc.ml
@@ -0,0 +1,31 @@
+open Ast
+
+(* added *)
+
+module StringHash = Hashtbl.Make(struct
+    type t = string           (* type of keys *)
+    let equal x y = x = y     (* use structural comparison *)
+    let hash = Hashtbl.hash   (* generic hash function *)
+  end);;
+
+let vals = StringHash.create 10;;
+
+(* done *)
+
+
+let rec eval = function 
+    Lit(x)            -> x
+  | Binop(e1, op, e2) ->
+      let v1  = eval e1 in
+      let v2 = eval e2 in
+      (match op with
+	Add -> v1 + v2
+      | Sub -> v1 - v2
+      | Mul -> v1 * v2
+      | Div -> v1 / v2)
+
+let _ =
+  let lexbuf = Lexing.from_channel stdin in
+  let expr = Parser.expr Scanner.tokenize lexbuf in
+  let result = eval expr in
+  print_endline (string_of_int result)

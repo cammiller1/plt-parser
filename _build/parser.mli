747b241db7e5b968e26b1cc9baeb66fac7bbcff0
Gabriel Clinger <gc2821@columbia.edu>
2021-04-21 18:11:49 -0400

    arrays

---
+++ _build/parser.mli
@@ -0,0 +1,47 @@
+type token =
+  | INT
+  | FLOAT
+  | BOOL
+  | STRING
+  | VOID
+  | ARRAY
+  | ASSIGN
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIVIDE
+  | EXP
+  | SEMC
+  | PP
+  | MM
+  | LT
+  | GT
+  | LTE
+  | GTE
+  | EQ
+  | NE
+  | AND
+  | OR
+  | MOD
+  | NOT
+  | IN
+  | IF
+  | ELSE
+  | WHILE
+  | FOR
+  | DEF
+  | RETURN
+  | LPAREN
+  | RPAREN
+  | RBRACE
+  | LBRACE
+  | COMMA
+  | ILITERAL of (int)
+  | FLITERAL of (string)
+  | ID of (string)
+  | SLITERAL of (string)
+  | BLITERAL of (bool)
+  | EOF
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast.program

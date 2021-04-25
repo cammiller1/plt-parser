4fadeda498198fd3978b18a097905e57c2e0fe4e
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2021-04-25 19:20:48 -0400

    removed unnecessary comment

--- codegen.ml
+++ codegen.ml
@@ -56,9 +56,6 @@ let translate (globals, functions, statements) =
   let main_t : L.lltype = 
       (* the [| and |] indicates an Ocaml array*)
       L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
-  (*  (* below LLVM's connection to a built-in function *)
-  let main_func : L.llvalue = 
-      L.declare_function "main" printf_t the_module in *)
 
 
   (* Define each function (arguments and return type) so we can 

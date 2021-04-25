6c8993e248823bf3d83d872dd03607b34e389a00
Cameron Miller <cm3959@columbia.edu>
2021-04-24 21:58:59 -0400

    wrote c file for concat

---
+++ built-in.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/*
+ * String concatenation
+ */
+
+char *concat(const char *s1, const char *s2)
+{
+	char *result = malloc(strlen(s1) + strlen(s2) + 1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
